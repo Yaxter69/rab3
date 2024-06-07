7a18e3dc4067bb3cb51c861394b40f24d20b38f9
Murenkiy Yakov <yaxter673@gmail.com>
2024-06-07 21:59:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Category } from './pages/Category';
+import { Home } from './pages/Home';
+import { Recipe } from './pages/Recipe';
+
+function NoMatch() {
+  let location = useLocation();
+  
+  return (
+    <>
+    <h1>Oops!</h1>
+    <h3>Sorry, an unexpected error has occurred</h3>
+    <h5>Does not exist: {location.pathname}</h5>
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <main className='container content'>
+          <Routes>
+          <Route path="*" element={<NoMatch />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="category/:name" element={<Category />} />
+            <Route path="meal/:id" element={<Recipe />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
